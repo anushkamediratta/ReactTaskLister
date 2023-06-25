8657bfcc9c7821c87d516aefdb762768f083547d
ANUSHKA <medirattaanushka23@gmail.com>
2023-06-26 01:09:01 +0530

    todolist

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import img from './itemlister.png'
+import { useState } from "react"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const App = () => {
+  const[todo,setTodo]= useState('')
+  const[todoList,setTodoList]=useState([])
+  console.log(todoList)
+  const onClickHandler=()=>{
+    let newtodoList=[...todoList]
+    let newItem ={
+      id:Math.random(),
+      value:todo
+    }
+    newtodoList.push(newItem)
+    setTodoList(newtodoList)
+    setTodo('')
+  }
+
+  const onDeleteHandler = (myId) => {
+    setTodoList(todoList.filter(item=> item.id!==myId))
+   
+  }
+
+  return <div>
+    <img src={img} width={100} height={100} className="logo"/>
+  <h1 className="app-title">React TaskManager</h1>
+  <div className="container">
+    Add Tasks 
+    <br/>
+    <input className="input-text" type="Text" placeholder="Enter Task" value={todo} onChange={e=> setTodo(e.target.value)}/>
+  <button type="button" className="add-btn" onClick={()=> onClickHandler()}>ADD</button>
+  <div className="list">
+    <br/>
+    <ul>
+   { 
+   todoList.map((item,index)=> {
+    return <div key={index}>
+
+     <li  onClick={()=>onDeleteHandler(item.id )}>
+      <input type="checkbox" name="" id="" checked={item.isDone}/>
+           {item.value}</li>
     </div>
-  );
+   })}
+   </ul>
+
+   </div>
+  </div>
+  
+  </div>
 }
 
-export default App;
+export default App
+
+
